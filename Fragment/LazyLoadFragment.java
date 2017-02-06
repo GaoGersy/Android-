a99f2cb40496119463ad133f2ddf545151782174
gersy <326609037@qq.com>
2017-02-06 20:32:05 +0800

    加入懒加载Fragment

---
+++ Fragment/LazyLoadFragment.java
@@ -0,0 +1,47 @@
+package com.gersion.androidcodewarehouse;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public abstract class LazyLoadFragment extends Fragment {
+
+    protected boolean isViewInitiated;
+    protected boolean isVisibleToUser;
+    protected boolean isDataInitiated;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isViewInitiated = true;
+        prepareFetchData();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        this.isVisibleToUser = isVisibleToUser;
+        prepareFetchData();
+    }
+
+    public boolean prepareFetchData() {
+        return prepareFetchData(false);
+    }
+
+    public boolean prepareFetchData(boolean forceUpdate) {
+        if (isVisibleToUser && isViewInitiated && (!isDataInitiated || forceUpdate)) {
+            fetchData();
+            isDataInitiated = true;
+            return true;
+        }
+        return false;
+    }
+
+    //实现懒加载里面的代码
+    public abstract void fetchData();
+
+}
